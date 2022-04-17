3c95ff0fea47ac6c4800f9372c1672f87aabf0ff
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2022-04-17 18:44:27 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "fontsource-roboto";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
