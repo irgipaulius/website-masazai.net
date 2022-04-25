2e6055c5af17aa45d51781c600f534a5a6ecedbe
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2022-04-25 15:05:24 +0300

    responsive ui

--- src/App.js
+++ src/App.js
@@ -1,92 +1,25 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import "fontsource-roboto";
-
-import PhoneIcon from "@material-ui/icons/Phone";
-import EmailIcon from "@material-ui/icons/Email";
-
-import NavBar from "./NavBar";
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <CssBaseline />
-      <NavBar />
-      <div
-        style={{
-          display: "flex",
-          height: "10vh",
-        }}
-      ></div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "5vh",
-        }}
-      >
-        <h1> Svetainė kuriama, </h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "5vh",
-        }}
-      >
-        <h1> atsiprašome už nepatogumus. </h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          height: "10vh",
-        }}
-      ></div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "10vh",
-        }}
-      >
-        <h2> Susisiekite su masažuotoja: </h2>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "5vh",
-        }}
-      >
-        <h2>Jelena Jacinkevičienė</h2>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "5vh",
-        }}
-      >
-        <PhoneIcon fontSize="large" color="primary" />
-        <h1> +37067273695 </h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "5vh",
-        }}
-      >
-        <EmailIcon fontSize="large" color="primary" />
-        <h1> jacinkevicienejelena@gmail.com</h1>
-      </div>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
