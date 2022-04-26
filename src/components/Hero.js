52ef5854a0c35383b98a58a0798d638f95634569
Albertas <albertas.rinkevicius@gmail.com>
2022-04-26 14:23:52 +0300

    Hero CSS update for all text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,9 +7,9 @@ export default function Hero() {
     <div className="hero-container">
       {/* <img className="background-image" src="/images/background.jpg" /> */}
       <div className="text-container">
-      <h1>MASAŽAS</h1>
-      <p className="subhead">PAS JELENĄ</p>
-      <p className="about">Profesionalus masažas Kaune</p>
+        <h1 className="title">MASAŽAS</h1>
+        <p className="title-subhead">PAS JELENĄ</p>
+        <p className="about">Profesionalus masažas Kaune</p>
       </div>
     </div>
   );
