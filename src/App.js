48fbffa843ce320121908e4a4be8b0d8b71f1b25
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2022-04-26 15:15:13 +0300

    fixed name mismatch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "fontsource-roboto";
 
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import "./Navbar.css";
+import "./NavBar.css";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
