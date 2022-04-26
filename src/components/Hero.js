1490fa286b383e4846d6fadcff52bde72f81dc22
Albertas <albertas.rinkevicius@gmail.com>
2022-04-26 12:53:41 +0300

    hero-container css start

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import "./Hero.css";
+
 export default function Hero() {
   return (
     <div className="hero-container">
-      <></>
+      {/* <img className="background-image" src="/images/background.jpg" /> */}
+      <div className="text-container">
+      <h1>MASAŽAS</h1>
+      <p className="subhead">PAS JELENĄ</p>
+      <p className="about">Profesionalus masažas Kaune</p>
+      </div>
     </div>
   );
 }
