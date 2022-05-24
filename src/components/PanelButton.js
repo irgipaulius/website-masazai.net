2fedd215668122d28b92b69fb791529e0240e0fa
Albertas <albertas.rinkevicius@gmail.com>
2022-05-24 21:48:47 +0300

    PanelButton CSS implementation v1

--- src/components/PanelButton.js
+++ src/components/PanelButton.js
@@ -6,6 +6,7 @@ import { PricingPanelContents } from "./panel/PricingPanel";
 import { ErrorPanelContents } from "./panel/ErrorPanel";
 
 import "rodal/lib/rodal.css";
+import styles from "./About.module.css";
 
 /**
  * @param {*} type "about" or "pricing"
@@ -43,34 +44,106 @@ export default function PanelButton({ text, type, focus = 0 }) {
 
   const setHidden = () => setVisible(false);
 
+  /*
+- Font sizes (px)
+10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98
+
+- Spacing system (px)
+2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128
+*/
+
+  const { about_btn } = styles;
+
+  const panel_container = {
+    backgroundColor: "lightgrey",
+    // color: "#333",
+  };
+  const panel_title = {
+    fontSize: "3rem",
+    textAlign: "center",
+    margin: "0 auto 3.2rem auto",
+    padding: "0 0 2.4rem 0",
+    color: "#333",
+    borderBottom: "3px solid #555",
+  };
+  const panel_menu_container = {
+    color: "red",
+    border: "1px dotted blue",
+
+    display: "flex",
+    gap: "2.4rem",
+  };
+  const panel_menu = {
+    listStyle: "none",
+
+    width: "30%",
+    color: "#555",
+
+    border: "1px solid grey",
+
+    display: "flex",
+    flexDirection: "column",
+    gap: "1.6rem",
+  };
+  const panel_item = {
+    border: "1px solid green",
+  };
+  const panel_link = {
+    fontSize: "2rem",
+    fontWeight: "600",
+    lineHeight: "1.1",
+    border: "1px solid orange",
+    cursor: "pointer",
+    display: "inline",
+    // width: "auto",
+  };
+
+  const panel_content = {
+    fontSize: "2rem",
+    lineHeight: "1.6",
+    width: "70%",
+
+    border: "1px solid red",
+  };
+
   return (
     <div>
-      <button onClick={toggleVisible}>{text}</button>
+      <button className={about_btn} onClick={toggleVisible}>
+        {text}
+      </button>
       <Rodal
+        customStyles={{
+          width: "80%",
+          height: "80%",
+          top: "11.2rem", // from top 11.2rem (48 + 8 + 56 = NavBar)
+
+          margin: "2.4rem auto 0 auto",
+          padding: "2.4rem",
+
+          borderRadius: "8px",
+        }}
+        //
         visible={visible}
         onClose={setHidden}
         closeOnEsc={true}
         animation="slideDown"
-        duration={1000}
+        duration={300}
       >
         {/* these classNames are placeholders. please fix them all. */}
-        <div className="panel_container">
-          <div className="panel_title">{title}</div>
-          <div className="panel_menu_container">
-            <ul className="panel_menu">
+        <div style={panel_container} className="panel_container">
+          <div style={panel_title}>{title}</div>
+          <div style={panel_menu_container}>
+            <ul style={panel_menu}>
               {Object.keys(contents).map((menuItem, index) => (
-                <li className="panel_item" key={index}>
-                  <div
-                    className="panel_link"
-                    onClick={() => setContentItem(menuItem)}
-                  >
+                <li style={panel_item} key={index}>
+                  <div style={panel_link} onClick={() => setContentItem(menuItem)}>
                     {menuItem}
                   </div>
                 </li>
               ))}
             </ul>
+            <div style={panel_content}>{contents[contentItem]}</div>
           </div>
-          <div className="panel_content">{contents[contentItem]}</div>
         </div>
       </Rodal>
     </div>
--- src/components/panel/AboutPanel.js
+++ src/components/panel/AboutPanel.js
@@ -1,9 +1,65 @@
 export const AboutPanelContents = {
   title: "title of indiscriminate nature",
   contents: {
-    "issilavinimas": (
+    issilavinimas: (
       <>
         <p style={{ color: "black" }}>content1</p>
+        {/* <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+        <p style={{ color: "black" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p> */}
       </>
     ),
     "kvalifikacijos kelimas": (
@@ -16,7 +72,7 @@ export const AboutPanelContents = {
         <p style={{ color: "green" }}>content3</p>
       </>
     ),
-    "igudziai": (
+    igudziai: (
       <>
         <p style={{ color: "blue" }}>content4</p>
       </>
