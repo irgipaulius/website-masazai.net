16cbf289f3cebc828d2c3a58ff8c39236ba3c5b1
Albertas <albertas.rinkevicius@gmail.com>
2022-05-31 10:36:51 +0300

    Splitted Main.js into Services.js and Popular.js

---
+++ src/components/Main/Popular.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import styles from "../Main.module.css";
+import { MainCheckmark } from "../MuiIcons";
+
+function Popular(visibility) {
+  const {
+    main_image_container,
+    points_container,
+    points_container_title,
+    points_text,
+    points_text_list_item,
+  } = styles;
+
+  return (
+    <>
+      <div className={main_image_container}>
+        <div className={points_container}>
+          <p className={points_container_title}>POPULIARIAUSI</p>
+          <ul className={points_text}>
+            <li className={points_text_list_item}>
+              <MainCheckmark />
+              <span>Klasikinis viso kūno masažas</span>
+            </li>
+            <li className={points_text_list_item}>
+              <MainCheckmark />
+              <span>Medaus masažas</span>
+            </li>
+            <li className={points_text_list_item}>
+              <MainCheckmark />
+              <span>Limfodrenažinis masažas</span>
+            </li>
+            <li className={points_text_list_item}>
+              <MainCheckmark />
+              <span>Chiromasažas masažas</span>
+            </li>
+            <li className={points_text_list_item}>
+              <MainCheckmark />
+              <span>Neurosedatyvinis-antistresinis masažas</span>
+            </li>
+            <li className={points_text_list_item}>
+              <MainCheckmark />
+              <span>Anticeliulitinis masažas</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Popular;
---
+++ src/components/Main/Services.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styles from "../Main.module.css";
+import PanelButton from "../PanelButton";
+
+function Services(visibility) {
+  const { main_text_container, main_texts, main_title, main_text } = styles;
+
+  return (
+    <>
+      <div className={main_text_container}>
+        <div className={main_texts}>
+          <p className={main_title}>Masažo paslaugos</p>
+          <p className={main_text}>
+            Siūlau pasirinkimą iš įvairių masažo technikų, bei galiu padėti išsirinkti tinkamą
+            procedūrą pagal asmeninius poreikius.
+          </p>
+          <p className={main_text}>Taip pat siūlau rekomendacijas masažo ilgalaikiams kursams. </p>
+          <p className={main_text}>
+            Šitą tekstą reikia patvarkyti, kol kas tiesiog prirašiau laikinų sakinių. Šita pastraipa
+            gali aprašyti daug įvairaus teksto apie siūlomas procedūras ir masažus, kurias galima
+            siūlyti klientams.
+          </p>
+          <p className={main_text}>Dėl kainų teiraukitės telefonu.</p>
+
+          <PanelButton text="Kainininkas" type="pricing" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Services;
