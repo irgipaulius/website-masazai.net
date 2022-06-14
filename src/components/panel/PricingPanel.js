6dfadba22eb490725ca326bb14f3afc9bd215a22
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2022-06-14 18:35:39 +0300

    Add photos

--- src/components/panel/PricingPanel.js
+++ src/components/panel/PricingPanel.js
@@ -1,12 +1,14 @@
 export const PricingPanelContents = {
   title: "Masažo paslaugos",
   contents: {
-    "Kainininkas": `# Kainos
+    //
+    Kainoraštis: `# Kainos
 |         Paslauga        |  Kaina  |
 |:-----------------------:|:-------:|
 | Chiromasazas            | 10 eur  |
 | Klasikinis kūno masažas | 50 eur  |
 | ...                     | ... eur |`,
+    //
     "Klasikinis kūno masažas": `# Klasikinis kūno masažas
 Tai viena iš efektyviausių priemonių gydant įvairias traumas ir susirgimus.
 
@@ -25,17 +27,27 @@ Atpalaiduoja sąnarius, panaikina raumenų ir kraujagyslių įtempimą, pagerina
 - Panaikinti sprando raumenų įtempimą
 - Panaikinti galvos skausmus
 - Atlaisvinti raumenų įtempimą`,
+    //
     "Neurosedatyvinis-antistresinis masažas": `# Neurosedatyvinis-antistresinis masažas
 
 Tai efektyvi masažo technika, išsiskirianti ypatingu raminamuoju (sedatyviniu) poveikiu, sinchronizuojanti abi kūno puses, kairiojo ir dešiniojo smegenų pusrutulių veiklą, pašalinanti kūno bei veido asimetriją. Šis masažas ypač padeda susidoroti su tokiais probleminiais veiksniais kaip chroniškas nuovargis, įvairios kilmės galvos skausmai, stresas, mimikos raumenų hiperaktyvumas, ankstyvas senėjimas.`,
+    //
     Chiromasažas: `# Chiromasažas
-Tai įvairių europietiškų bei rytietiškų masažo sistemų, amerikietiškos chiro praktikos ir kineziologijos sintezė. Kiekvienam klientui parenkami individualūs masažo elementai. Priklausomai nuo situacijos, gali būti masažuojama ne tik delnais, pirštų pagalvėlėmis, bet ir alkūnėmis bei dilbiais. Jis ramina, sumažina nervinę įtampą, nemigą, pagerina kraujotaką, raumenų tonusą, medžiagų apytaką, išnyksta patinimai, skausmai, nuovargis, pagerėja nuotaika, bendra savijauta, darbingumas.`,
+Tai įvairių europietiškų bei rytietiškų masažo sistemų, amerikietiškos chiro praktikos ir kineziologijos sintezė. Kiekvienam klientui parenkami individualūs masažo elementai. Priklausomai nuo situacijos, gali būti masažuojama ne tik delnais, pirštų pagalvėlėmis, bet ir alkūnėmis bei dilbiais. Jis ramina, sumažina nervinę įtampą, nemigą, pagerina kraujotaką, raumenų tonusą, medžiagų apytaką, išnyksta patinimai, skausmai, nuovargis, pagerėja nuotaika, bendra savijauta, darbingumas.
+
+![Chiromasažas](images/chiromasazas.jpg "Chiromasažas")
+`,
+    //
     "Klasikinis-segmentinis masažas": `# Klasikinis segmentinis masažas 
 Segmentinis masažas yra seniausias sveikatos higienos ir gydymo menas. Naudojamas kaip gydymo taip ir profilaktiniais tikslais. Šis masažas idealiai tinka žmonėms, dirbantiems sėdimą ar sunkų fizinį darbą, kai visa įtampa susikaupia įvairiose stuburo vietose, kaklo ir pečių srityje ir dėl to susiformuoja skausmingi raumenų mazgai. Jo metu atliekami specialūs glostymo, trynimo, maigymo bei vibracijų veiksmai. Šio masažo metu pasišalina mirusios odos ląstelės, pagerėja odos kvėpavimas, padidėja raumenų susitraukimo funkcija bei suaktyvėja medžiagų apykaita. Periodiškai atliekamas masažas padės atsikratyti dažnų nugaros ir galvos skausmų, atpalaiduos įtemptas raumenų grupes, jas stiprins. Segmentinis masažas daug kartų efektyvesnis už tradicinį masažą.
 
 ![Klasikinis segmentinis masazas](images/klasikinis-segmentinis.jpg "Klasikinis segmentinis  masazas")`,
+    //
     "Limfodrenažinis masažas": `# Limfodrenažinis masažas
-**Limfinė sistema** - svarbi organizmo dalis, padedanti apsaugoti organizmą nuo įvairių infekcijų, kenksmingų aplinkos veiksnių bei valyti visą kūną. Sutrikusi šios sistemos veikla gali sukelti įvairius sveikatos negalavimus ar odos nelygumus, pavyzdžiui, venų varikozę, celiulitą, prastą emocinę būseną, energijos stoką ar padidėjusį kraujo spaudimą. Puikus būdas išvengti šių sveikatos sutrikimų – limfodrenažinis masažas, kuris padeda ne tik nugalėti jau esamas problemas, bet ir užkirsti kelią naujų atsiradimui. Limfodrenažinis kūno masažas, atliekamas lėtais, ilgais bei giliais judesiais limfos tekėjimo kryptimis, padeda suaktyvinti šio skysčio cirkuliaciją. Teigiamas masažo poveikis yra jaučiamas visame organizme. Limfodrenažinis masažas efektyviai paspartina susikaupusių toksinų pašalinimą bei padidina viso organizmo tonusą. Dėl šios priežasties suaktyvėja ląstelių atsinaujinimo procesai bei gražėja oda, mažėja apimtys, pamažu nyksta celiulitas.`,
+**Limfinė sistema** - svarbi organizmo dalis, padedanti apsaugoti organizmą nuo įvairių infekcijų, kenksmingų aplinkos veiksnių bei valyti visą kūną. Sutrikusi šios sistemos veikla gali sukelti įvairius sveikatos negalavimus ar odos nelygumus, pavyzdžiui, venų varikozę, celiulitą, prastą emocinę būseną, energijos stoką ar padidėjusį kraujo spaudimą. Puikus būdas išvengti šių sveikatos sutrikimų – limfodrenažinis masažas, kuris padeda ne tik nugalėti jau esamas problemas, bet ir užkirsti kelią naujų atsiradimui. Limfodrenažinis kūno masažas, atliekamas lėtais, ilgais bei giliais judesiais limfos tekėjimo kryptimis, padeda suaktyvinti šio skysčio cirkuliaciją. Teigiamas masažo poveikis yra jaučiamas visame organizme. Limfodrenažinis masažas efektyviai paspartina susikaupusių toksinų pašalinimą bei padidina viso organizmo tonusą. Dėl šios priežasties suaktyvėja ląstelių atsinaujinimo procesai bei gražėja oda, mažėja apimtys, pamažu nyksta celiulitas.
+
+![Limfodrenažinis masažas](images/limfodrenazinis.jpeg "Limfodrenažinis masažas")`,
+    //
     "Nėščiųjų masažas": `# Nėščiųjų masažas
 Dažnai nėštumo metu būsimos mamytes tampa jautresnes, kartais ir nervingesnes, todėl masažas gali padėti išsklaidyti blogas emocijas, atsipalaiduoti ir dar daugiau džiaugtis šiuo gražiu etapu.
 
@@ -45,13 +57,23 @@ Dažnai nėštumo metu būsimos mamytes tampa jautresnes, kartais ir nervingesne
 - Pagerina raumenų ir sąnarių funkcijas.
 - Gerina medžiagų apykaitą, todėl nėštumo laikotarpiu mažėja sąnarių patinimas (edema).
 - Švelnūs masažisto prisilietimai teigiamai veikia psichinę ir emocinę būsimos mamos būseną, padeda pašalinti psichinį ir fizinį nuovargį.
-- Mažina gimdymo komplikacijų tikimybę.`,
+- Mažina gimdymo komplikacijų tikimybę.
+
+![Nėščiųjų masažas](images/pregnant.jpg "Nėščiųjų masažas")`,
+    //
     "Atpalaiduojantis viso kūno masažas": `# Atpalaiduojantis masažas
 Viena populiariausių masažo formų visame pasaulyje. Tai ypatingai švelni, raminanti ir kūno harmoniją suteikianti procedūra, kuri padės Jums pailsėti po dienos darbų, nuimti stresą, įtampą ir pasinerti į teigiamų emocijų sūkurį. Šios rūšies masažą rekomenduojame žmonėms, kurie nuolat stresuoja, dirba sunkų protinį ar fizinį darbą, jaučiasi viskam apatiški, nuolatos pavargę, nervingi ir sunkiai atsipalaiduoja. Masažo metu stimuliuojami tam tikri smegenų centrai, todėl kūnas išsiskiria vadinamąjį laimės hormoną endrofiną, kuris padeda nusiraminti, pasisemti pozityvios energijos ir atgauti sielos pusiausvyrą. Vienas ar du šio atpalaiduojančio masažo seansai per savaitę garantuoja Jums gerą darbingumą, puikią nuotaiką bei savijautą. Masažas taip pat gerina kraujo cirkuliaciją, medžiagų apykaitą, atstato (regeneruoja) nuilsusius raumenis, todėl puikiai atgaivins po sunkios darbo dienos, padės atsipalaiduoti ir susigrąžinti prarastas dvasines bei fizines jėgas.`,
+    //
     "Anticeliulitinis masažas rankomis": `# Anticeliulitinis masažas rankomis
-Tai ypač efektyvus kovos su celiulitu būdas, taikomas tam tikrų probleminių sričių – šlaunų, sėdmenų ir pilvo – netolygiam poodiniam sluksniui (celiulitui) mažinti. Masažas atliekamas naudojant specifinius gilius masazo judesius, padedančius pašalinti celiulitą,s uskaidyti riebalines medžiagas ir tokiu būdu sumažinti kūno apimtis. Anticeliulitinio masažo metu oda įšildoma, skatinama medžiagų apykaita, kraujotaka, šalinamos toksinės medžiagos iš oragnizmo, oda tonizuojama, lyginama ir stangrinama. Anticeliulitinio masažo metu celiulitiniuose audiniuose sukeliama aremija ir srovenantis kraujas padeda pašalinti susikaupusius riebalus iš riebalinių ląstelių. Procedūros metu taip pat aktyvinama kraujotaka, šalinamas toksinų perteklius, o audiniai aprūpinami šviežiu deguonimi ir reikalingomis maisto medžiagomis. Intensyvūs masažuojamieji veiksmai padeda aktyvinti limfą, reguliuoja skysčių cirkuliaciją Jūsų organizme.`,
+Tai ypač efektyvus kovos su celiulitu būdas, taikomas tam tikrų probleminių sričių – šlaunų, sėdmenų ir pilvo – netolygiam poodiniam sluksniui (celiulitui) mažinti. Masažas atliekamas naudojant specifinius gilius masazo judesius, padedančius pašalinti celiulitą,s uskaidyti riebalines medžiagas ir tokiu būdu sumažinti kūno apimtis. Anticeliulitinio masažo metu oda įšildoma, skatinama medžiagų apykaita, kraujotaka, šalinamos toksinės medžiagos iš oragnizmo, oda tonizuojama, lyginama ir stangrinama. Anticeliulitinio masažo metu celiulitiniuose audiniuose sukeliama aremija ir srovenantis kraujas padeda pašalinti susikaupusius riebalus iš riebalinių ląstelių. Procedūros metu taip pat aktyvinama kraujotaka, šalinamas toksinų perteklius, o audiniai aprūpinami šviežiu deguonimi ir reikalingomis maisto medžiagomis. Intensyvūs masažuojamieji veiksmai padeda aktyvinti limfą, reguliuoja skysčių cirkuliaciją Jūsų organizme.
+
+![Anticeliulitinis masažas](images/anti_celiulitinis.jpg "Anticeliulitinis masažas")`,
+    //
     "Medaus masažas": `# Medaus masažas
-Šio masažo metu skatinama audinių regeneracija ir stimuliuojama imuninė sistema. Medaus masažas atstoja viso kūno pilingą. Medus išvalo ir atveria odos poras, ją regeneruoja, šalina visą suragėjusį sluoksnį, maitina bei valo šlakus. Oda tampa švari, švelni, prisotinta deguonimi ir meduje esančiomis vertingomis maistinėmis medžiagomis. Masažuotojo judesiai ir medaus tąsumas aktyvina kraujotaką. Šio masažo metu skatinama audinių regeneracija ir stimuliuojama imuninė sistema. Po šio masažo, kurį laiką jaučiama maloni šiluma.`,
+Šio masažo metu skatinama audinių regeneracija ir stimuliuojama imuninė sistema. Medaus masažas atstoja viso kūno pilingą. Medus išvalo ir atveria odos poras, ją regeneruoja, šalina visą suragėjusį sluoksnį, maitina bei valo šlakus. Oda tampa švari, švelni, prisotinta deguonimi ir meduje esančiomis vertingomis maistinėmis medžiagomis. Masažuotojo judesiai ir medaus tąsumas aktyvina kraujotaką. Šio masažo metu skatinama audinių regeneracija ir stimuliuojama imuninė sistema. Po šio masažo, kurį laiką jaučiama maloni šiluma.
+
+![Medaus masažas](images/honey.webp "Medaus masažas")`,
+    //
     'Veido masažas "Švytintis veidas"': `# **Švytintis veidas**
 Klasikinis veido masažas – vienas iš efektyviausių būdų išlaikyti sveiką, jaunatvišką odos išvaizdą. Tai malonus ir labai naudingas masažas įvairiais amžiaus tarpsniais, kadangi masažas veikia tiek prevenciškai (užkerta kelią odos senėjimui), tiek ir jau matomus odos senėjimo veiksnius, tokius kaip negyvybinga odos spalva, mimikos raukšlės, odos tonuso ar elastingumo sumažėjimas. Masažo metu atliekami glostymo, trynimo, maigymo judesiai, kurie aktyvina kraujotaką ir limfotaką, o to pasekoje gerėja maistinių medžiagų ir deguonies apykaita, gerėja toksinų ir šlakų pasišalinimas. Taip pat masažo metu veikiami ir poodiniai audiniai, nervai bei raumenys, taigi jaučiamas ne tik raumenų, bet ir bendras organizmo atsipalaidavimas.
 
@@ -61,8 +83,13 @@ Klasikinis veido masažas – vienas iš efektyviausių būdų išlaikyti sveik�
 2. etapas – rekomenduojama atlikti odos šveitimą (pašalinamos negyvos odos ląstelės)
 3. etapas – atliekamas klasikinis veido ir kaklo masažas
 4. etapas – dedama veido kaukė (ramina, drėkina odą, sutiprina atlikto masažo rezultatus)
-5. etapas – procedūros pabaiga (tepamas paakių kremas, serumas ir kremas, atitinkantys odos poreikius)`,
+5. etapas – procedūros pabaiga (tepamas paakių kremas, serumas ir kremas, atitinkantys odos poreikius)
+
+![Veido masažas](images/veido.jpg 'Veido masažas "Švytintis veidas"')`,
+    //
     '"Su džok" terapija': `# "Su džok" terapija
-Korėjietiška taškinio delnų masažo sistema. Visi kūno organų, stuburo, sanarių ir kiti taškai yra jūsų delne. Žinodami kokius taškus spaudyti jūs galėsite gydyti save kur bebūtumėt. Delnas - gydytojas visada su jumis. Skaudūs taškai delne rodo ne tik dabartines jūsų ligas, silpnas jūsų organizmo vietas, bet taip pat ir ateinančias ligas. Tiksliai veikiant taškus, įvairūs skausmai pradeda mažėti jau po kelių minučių. Be abejo reikia žinoti kaip, kiek laiko ir kokius taškus veikti. Tam nereikia išankstinio tikėjimo, bet būtina pabandyti, kad įsitikintumėt šios terapijos unikalumu. Už savo sveikatą esate atsakingi tik jūs patys. Ir taškų delnuose pagalba jūs galite efektyviai sau padėti spręsti savo sveikatos problemas, o gerai išmokę su džok terapijos principų galėsite ir kitiems pagelbėti.`,
+Korėjietiška taškinio delnų masažo sistema. Visi kūno organų, stuburo, sanarių ir kiti taškai yra jūsų delne. Žinodami kokius taškus spaudyti jūs galėsite gydyti save kur bebūtumėt. Delnas - gydytojas visada su jumis. Skaudūs taškai delne rodo ne tik dabartines jūsų ligas, silpnas jūsų organizmo vietas, bet taip pat ir ateinančias ligas. Tiksliai veikiant taškus, įvairūs skausmai pradeda mažėti jau po kelių minučių. Be abejo reikia žinoti kaip, kiek laiko ir kokius taškus veikti. Tam nereikia išankstinio tikėjimo, bet būtina pabandyti, kad įsitikintumėt šios terapijos unikalumu. Už savo sveikatą esate atsakingi tik jūs patys. Ir taškų delnuose pagalba jūs galite efektyviai sau padėti spręsti savo sveikatos problemas, o gerai išmokę su džok terapijos principų galėsite ir kitiems pagelbėti.
+
+!["Su džok" terapija](images/su_dzok.jpg '"Su džok" terapija')`,
   },
 };
