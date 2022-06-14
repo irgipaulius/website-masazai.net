a62712175b86eebd624163cde04d10dde9d9bf8f
Paulius Jacinkevicius <paulius.jacinkevicius@cdn77.com>
2022-06-14 11:12:25 +0300

    Add modal text

--- src/components/panel/AboutPanel.js
+++ src/components/panel/AboutPanel.js
@@ -1,101 +1,36 @@
 export const AboutPanelContents = {
-  title: "title of indiscriminate nature",
+  title: "Masažuotojos kvalifikacija",
   contents: {
-    issilavinimas: `content1`,
-    "kvalifikacijos kelimas": `A paragraph with *emphasis* and **strong importance**.
-
-> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
-
-* Lists
-* [ ] todo
-* [x] done
-
-A table:
-
-| a | b |
-| - | - |
-`,
-    "darbo patirtis": `
-# Header 1
-## Header 2
-
-_italic_
-
-**bold**
-
-<b>bold Html</b>
-    `,
-    igudziai: "# header1\n*italic*",
-    "taip toliau": `# Quae luctuque inportuna lapsae medios
-
-## Tam sequi sustinet mihi nomina prius committere
-
-Lorem markdownum mendacia, ense freta ingreditur potius, non canisve, esse
-iuvenci Penei discedite parsque, vidi carens. Parce pedem vagus eram modo
-crinalem et auras, [altera pectore](http://vetantis-locus.io/cum). Habeto dixit
-et auget comitem nunc obliquo; nihil lecta, in. Tu repulsa petit diffugiunt
-nobis sollicitatque unda melior proles expetit; fidesque eripiunt nulla; in
-visum. Exemploque nec oscula coegi bacis.
-
-    if (flash.link(pitch, directory, timeDriveRipcording) + rawMysqlIpv <=
-            isdn_system) {
-        illegal_wordart.redundancyAddress.cdCaptchaModifier(nosqlAddressNoc, and
-                / 564632);
-        text_brouter_bespoke *= cms_key(drop, page_ram /
-                peripheral_trojan_external);
-    }
-    device.verticalLun.vpi(impact, link, supplyReadmeOn);
-    if (marketForumToolbar(interlaced, download)) {
-        modifier_animated = 24;
-        mysqlVdu = internalBit(soap, 5, 2) +
-                drive_opengl_autoresponder.hibernateOopTelecommunications(
-                icsNon);
-    } else {
-        trojan_favorites += surge;
-    }
-    if (forum * -1 * 4) {
-        interface_pci_state += leak;
-    } else {
-        wampFreeware.ciscDropEcc.goldenTopologyBridge(megabitComputerSchema,
-                dataCrossCd);
-        plagiarism_dma_cloud = access_publishing_leak + station_plagiarism +
-                protector + hashtag;
-    }
-
-## Coniuge e vincere mirarique quoque eo nil
-
-Iovis sub dederunt iunctus inde nox evitabile arva tamen donata. Erat detis,
-ducibusque aguntur frondes, tenet mihi, futuri sit. Blanditur amentis, tormentis
-ceras viribus. Qui venit oras ast forem igni tempora gladios currere pedibus
-robore **gelidos** matrona **ac extimuit humani** acta delubris; et. His quae
-cervos exspectatum claro, venit, procorum sit silva meo referuntur haec
-**dextera**.
-
-## Accepta Amor tibi arces palustri passim numero
-
-Captis nos vanum, ibi tot tardarunt matrem putares, caeruleos suis plura
-[sinuatus misit igne](http://www.teli.com/spatiosoultra.aspx) flamma illo
-dubitat. Attonitum percepit adunco abstitimus voce.
-
-## Celebrabere munus quaesiti caeli dulcedine ea quae
-
-Cornibus tradere extulerat nata: matrem deploravit verba sperabam vultu,
-effugies monet gravitate unus tenentem. Contento sanguine inmemores excidit
-manuque mihi **adiit manusque videntur** praecordia tamen referre. Quae deorum
-obvia, an tuum; erit smaragdis illic [ad](http://www.proterviset.org/siquid)
-illo quae Scythicae aevo uberibus tenetur!
-
-Locis pulsatus, externis atris quoque Pyreneus purpureasque nunc Athin spatium
-cunctosque iacebat nautae, guttura. Ullis et acre gemmantia audito, postquam
-tardus etiam malo catulo **quantum sit**, hac quas fulgentem, Curetida; manu.
-Petit ingentia longoque **haut hiatus altera**, vellens Tritonida est deduxisse
-genua tellus: ait? Longis ingemuit distantes condidit reppulit!
-
-Iubebat satis tormentis procul tamen dictoque **Curetas oblita**; et Argo dei
-**vulnera petit**, ostentare! Gloria tangit restabas; igni nostro, *vestro
-multifidasque unde*. Saturnia figuram Ortygiam agros. Procubuit pactaque et
-subdita sanguine ictibus Ino exponit, instruitur dolendi. Quamvis indigenis
-patriis est postquam vixque nectare et ictu frustra manibus bos nullo parilique
-volui.`,
+    'Išsilavinimas ir kvalifikacija': `
+| Data        |                       Lokacija                       | Kvalifikacijos kėlimo įstaiga                                                                     |
+|:-----------:|:----------------------------------------------------:|---------------------------------------------------------------------------------------------------|
+| 2021        |                      Nuotolinis                      | Klinikiniai masažai. Kaklinės ir krūtininės stuburo dalies patologijos                            |
+| 2021        | Nuotolinis                                           | Nugaros skausmas gydytojo, kineziterapeuto, ergoterapeuto ir masažuotojo praktikoje               |
+| 2021        | Nuotolinis                                           | Miofascijinių ir giliųjų audinių masažo technikų taikymas esant išialgijai                        |
+| 2018        | Vilnius                                              | FOREVER AKADEMIJA                                                                                 |
+| 2013        | Kaunas                                               | "SU DŽOK" terapija                                                                                |
+| 2009        | Kaunas                                               | "STARVAC SP2" prancūziškas aparatas, anticeliulitinė programa ir veido vakuuminis masažas, kursai |
+| 2009        | Vilnius                                              | "Biodroga Academy for Estetic Sciences and Spa Therapy"                                           |
+| 2009        | Kaunas                                               | Seminaras "Veido kūno ir plaukų priežiūra" su JERICHO produktais                                  |
+| 2006        | Ajurvedos stovykla-seminaras                         | "Ajurveda. Praeities patirtis šiandien"                                                           |
+| 2005        | SDTS centras                                         | Masažas pediatrijoje (masažuotojams, kineziterapeuto padėjėjams)                                  |
+| 2004        | SDTS centras                                         | Masažas pediatrijoje (masažuotojams, kineziterapeuto padėjėjams)                                  |
+| 2003        | SDTS centras                                         | Masažas (masažuotojams, kineziterapeuto padejėjams)                                               |
+| 2002        | SDTS centras                                         | Manualinės terapijos pagrindai, masažas                                                           |
+| 1993        | UAB "Virginija" Vilniaus psichines sveikatos Centras | Praktinės psichologijos instruktorių kursai, Kijevas                                              |
+| 1988        | LVKKI Visuomeninių profesijų fakultetas              | Visuomeninė sportinio masažo specialybė                                                           |
+| 1985 - 1987 | Kauno P. Mažylio medicinos mokykla                   |                                                                                                   |`,
+    "Darbo stažas": `
+| Laikotarpis |                                                                                        Darbovietė                                                                                       |
+|:-----------:|:---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 2015 -      | Individuali fizinės gerovės užtikrinimo veikla pagal pažymą **Nr.643944**.                                                                                                              |
+| 2009 - 2015 | **UAB *ERIKOS Grožio Salonas*** - masažuotoja-distributorė: klasikinio, profilaktinio, vakuuminio-aparatino, kosmetinio masažo atlikimas, produktų platinimas; masažo apmokymai.        |
+| 2007 - 2009 | **UAB *Grožio Studija ELROJUS***- masažuotoja: klasikinio, profilaktinio, kosmetinio masažo atlikimas; masažo apmokymai.                                                                |
+| 2006 - 2007 | Masažo kabinetas - masažuotoja: masažo paslaugų teikimas.                                                                                                                               |
+| 2002 - 2006 | Verslo liudijimas - masažuotoja: klasikinio, profilaktinio, kosmetinio masažo atlikimas.                                                                                                |
+| 1995 - 2000 | J.Jacinkevičienės **IĮ *Gera Diena***, savininkė. Masažo salonas ***ČIA IR DABAR***. Lygiagrečiai - masažuotojos praktika.                                                              |
+| 1992 - 1993 | **UAB *ŽAVESYS*** - masažuotoja: klasikinio, profilaktinio, kosmetinio masažo atlikimas. Licencija privačiam masažui atlikti.                                                           |
+| 1990 - 1992 | Statybinių medžiagų įmonė ***GRANITAS*** felčerė-masažuotoja: fizioterapinių procedūrų, klasikinio masažo atlikimas. Lygiagrečiai, - masažuotoja ***SILVOS*** fabriko medicinos punkte. |
+| 1987 - 1990 | ***Lietuvos Kūno kultūros* institutas** - laborantė-masažuotoja: biocheminiai tyrimai, injekcijos, sportinio masažo atlikimas.                                                          |`,
   },
 };
--- src/components/panel/PricingPanel.js
+++ src/components/panel/PricingPanel.js
@@ -1,7 +1,60 @@
 export const PricingPanelContents = {
-  title: "title of superior persuasion",
+  title: "Masažo paslaugos",
   contents: {
-    "masazas rankom": `100 euru`,
-    "masazas kojom": `200 euru`,
+    "Klasikinis kūno masažas": `# Klasikinis kūno masažas
+Tai viena iš efektyviausių priemonių gydant įvairias traumas ir susirgimus.
+
+Klasikinis masažas padeda profilaktiškai gydyti osteochondrozę, artritą, nugaros, kaklo ir liemens skausmus.
+Visuomet naudojamas po įvairių traumų, lūžių, sutrenkimų. Panaikina galvos skausmą ir raumenų įtempimą.
+
+
+## Rezultatai:
+
+Atpalaiduoja sąnarius, panaikina raumenų ir kraujagyslių įtempimą, pagerina laikyseną ir sustiprina dėmesį bei susikaupimą.
+
+
+### Klasikinis kūno masažas Jums tinka, jei norite:
+
+- Pašalinti skausmus nugaroje
+- Panaikinti sprando raumenų įtempimą
+- Panaikinti galvos skausmus
+- Atlaisvinti raumenų įtempimą`,
+    "Neurosedatyvinis-antistresinis masažas": `# Neurosedatyvinis-antistresinis masažas
+
+Tai efektyvi masažo technika, išsiskirianti ypatingu raminamuoju (sedatyviniu) poveikiu, sinchronizuojanti abi kūno puses, kairiojo ir dešiniojo smegenų pusrutulių veiklą, pašalinanti kūno bei veido asimetriją. Šis masažas ypač padeda susidoroti su tokiais probleminiais veiksniais kaip chroniškas nuovargis, įvairios kilmės galvos skausmai, stresas, mimikos raumenų hiperaktyvumas, ankstyvas senėjimas.`,
+    Chiromasažas: `# Chiromasažas
+Tai įvairių europietiškų bei rytietiškų masažo sistemų, amerikietiškos chiro praktikos ir kineziologijos sintezė. Kiekvienam klientui parenkami individualūs masažo elementai. Priklausomai nuo situacijos, gali būti masažuojama ne tik delnais, pirštų pagalvėlėmis, bet ir alkūnėmis bei dilbiais. Jis ramina, sumažina nervinę įtampą, nemigą, pagerina kraujotaką, raumenų tonusą, medžiagų apytaką, išnyksta patinimai, skausmai, nuovargis, pagerėja nuotaika, bendra savijauta, darbingumas.`,
+    "Klasikinis-segmentinis masažas": `# Klasikinis segmentinis masažas 
+Segmentinis masažas yra seniausias sveikatos higienos ir gydymo menas. Naudojamas kaip gydymo taip ir profilaktiniais tikslais. Šis masažas idealiai tinka žmonėms, dirbantiems sėdimą ar sunkų fizinį darbą, kai visa įtampa susikaupia įvairiose stuburo vietose, kaklo ir pečių srityje ir dėl to susiformuoja skausmingi raumenų mazgai. Jo metu atliekami specialūs glostymo, trynimo, maigymo bei vibracijų veiksmai. Šio masažo metu pasišalina mirusios odos ląstelės, pagerėja odos kvėpavimas, padidėja raumenų susitraukimo funkcija bei suaktyvėja medžiagų apykaita. Periodiškai atliekamas masažas padės atsikratyti dažnų nugaros ir galvos skausmų, atpalaiduos įtemptas raumenų grupes, jas stiprins. Segmentinis masažas daug kartų efektyvesnis už tradicinį masažą.`,
+    "Limfodrenažinis masažas": `# Limfodrenažinis masažas
+**Limfinė sistema** - svarbi organizmo dalis, padedanti apsaugoti organizmą nuo įvairių infekcijų, kenksmingų aplinkos veiksnių bei valyti visą kūną. Sutrikusi šios sistemos veikla gali sukelti įvairius sveikatos negalavimus ar odos nelygumus, pavyzdžiui, venų varikozę, celiulitą, prastą emocinę būseną, energijos stoką ar padidėjusį kraujo spaudimą. Puikus būdas išvengti šių sveikatos sutrikimų – limfodrenažinis masažas, kuris padeda ne tik nugalėti jau esamas problemas, bet ir užkirsti kelią naujų atsiradimui. Limfodrenažinis kūno masažas, atliekamas lėtais, ilgais bei giliais judesiais limfos tekėjimo kryptimis, padeda suaktyvinti šio skysčio cirkuliaciją. Teigiamas masažo poveikis yra jaučiamas visame organizme. Limfodrenažinis masažas efektyviai paspartina susikaupusių toksinų pašalinimą bei padidina viso organizmo tonusą. Dėl šios priežasties suaktyvėja ląstelių atsinaujinimo procesai bei gražėja oda, mažėja apimtys, pamažu nyksta celiulitas.`,
+    "Nėščiųjų masažas": `# Nėščiųjų masažas
+Dažnai nėštumo metu būsimos mamytes tampa jautresnes, kartais ir nervingesnes, todėl masažas gali padėti išsklaidyti blogas emocijas, atsipalaiduoti ir dar daugiau džiaugtis šiuo gražiu etapu.
+
+## Nėščiųjų masažo nauda:
+- Stimuliuoja minkštuosius audinius.
+- Mažina skysčių kaupimąsi sąnariuose.
+- Pagerina raumenų ir sąnarių funkcijas.
+- Gerina medžiagų apykaitą, todėl nėštumo laikotarpiu mažėja sąnarių patinimas (edema).
+- Švelnūs masažisto prisilietimai teigiamai veikia psichinę ir emocinę būsimos mamos būseną, padeda pašalinti psichinį ir fizinį nuovargį.
+- Mažina gimdymo komplikacijų tikimybę.`,
+    "Atpalaiduojantis viso kūno masažas": `# Atpalaiduojantis masažas
+Viena populiariausių masažo formų visame pasaulyje. Tai ypatingai švelni, raminanti ir kūno harmoniją suteikianti procedūra, kuri padės Jums pailsėti po dienos darbų, nuimti stresą, įtampą ir pasinerti į teigiamų emocijų sūkurį. Šios rūšies masažą rekomenduojame žmonėms, kurie nuolat stresuoja, dirba sunkų protinį ar fizinį darbą, jaučiasi viskam apatiški, nuolatos pavargę, nervingi ir sunkiai atsipalaiduoja. Masažo metu stimuliuojami tam tikri smegenų centrai, todėl kūnas išsiskiria vadinamąjį laimės hormoną endrofiną, kuris padeda nusiraminti, pasisemti pozityvios energijos ir atgauti sielos pusiausvyrą. Vienas ar du šio atpalaiduojančio masažo seansai per savaitę garantuoja Jums gerą darbingumą, puikią nuotaiką bei savijautą. Masažas taip pat gerina kraujo cirkuliaciją, medžiagų apykaitą, atstato (regeneruoja) nuilsusius raumenis, todėl puikiai atgaivins po sunkios darbo dienos, padės atsipalaiduoti ir susigrąžinti prarastas dvasines bei fizines jėgas.`,
+    "Anticeliulitinis masažas rankomis": `# Anticeliulitinis masažas rankomis
+Tai ypač efektyvus kovos su celiulitu būdas, taikomas tam tikrų probleminių sričių – šlaunų, sėdmenų ir pilvo – netolygiam poodiniam sluksniui (celiulitui) mažinti. Masažas atliekamas naudojant specifinius gilius masazo judesius, padedančius pašalinti celiulitą,s uskaidyti riebalines medžiagas ir tokiu būdu sumažinti kūno apimtis. Anticeliulitinio masažo metu oda įšildoma, skatinama medžiagų apykaita, kraujotaka, šalinamos toksinės medžiagos iš oragnizmo, oda tonizuojama, lyginama ir stangrinama. Anticeliulitinio masažo metu celiulitiniuose audiniuose sukeliama aremija ir srovenantis kraujas padeda pašalinti susikaupusius riebalus iš riebalinių ląstelių. Procedūros metu taip pat aktyvinama kraujotaka, šalinamas toksinų perteklius, o audiniai aprūpinami šviežiu deguonimi ir reikalingomis maisto medžiagomis. Intensyvūs masažuojamieji veiksmai padeda aktyvinti limfą, reguliuoja skysčių cirkuliaciją Jūsų organizme.`,
+    "Medaus masažas": `# Medaus masažas
+Šio masažo metu skatinama audinių regeneracija ir stimuliuojama imuninė sistema. Medaus masažas atstoja viso kūno pilingą. Medus išvalo ir atveria odos poras, ją regeneruoja, šalina visą suragėjusį sluoksnį, maitina bei valo šlakus. Oda tampa švari, švelni, prisotinta deguonimi ir meduje esančiomis vertingomis maistinėmis medžiagomis. Masažuotojo judesiai ir medaus tąsumas aktyvina kraujotaką. Šio masažo metu skatinama audinių regeneracija ir stimuliuojama imuninė sistema. Po šio masažo, kurį laiką jaučiama maloni šiluma.`,
+    'Veido masažas "Švytintis veidas"': `# **Švytintis veidas**
+Klasikinis veido masažas – vienas iš efektyviausių būdų išlaikyti sveiką, jaunatvišką odos išvaizdą. Tai malonus ir labai naudingas masažas įvairiais amžiaus tarpsniais, kadangi masažas veikia tiek prevenciškai (užkerta kelią odos senėjimui), tiek ir jau matomus odos senėjimo veiksnius, tokius kaip negyvybinga odos spalva, mimikos raukšlės, odos tonuso ar elastingumo sumažėjimas. Masažo metu atliekami glostymo, trynimo, maigymo judesiai, kurie aktyvina kraujotaką ir limfotaką, o to pasekoje gerėja maistinių medžiagų ir deguonies apykaita, gerėja toksinų ir šlakų pasišalinimas. Taip pat masažo metu veikiami ir poodiniai audiniai, nervai bei raumenys, taigi jaučiamas ne tik raumenų, bet ir bendras organizmo atsipalaidavimas.
+
+### Procedūros eiga:
+
+1. etapas – makiažo nuvalymas (oda paruošiama procedūrai)
+2. etapas – rekomenduojama atlikti odos šveitimą (pašalinamos negyvos odos ląstelės)
+3. etapas – atliekamas klasikinis veido ir kaklo masažas
+4. etapas – dedama veido kaukė (ramina, drėkina odą, sutiprina atlikto masažo rezultatus)
+5. etapas – procedūros pabaiga (tepamas paakių kremas, serumas ir kremas, atitinkantys odos poreikius)`,
+    '"Su džok" terapija': `# "Su džok" terapija
+Korėjietiška taškinio delnų masažo sistema. Visi kūno organų, stuburo, sanarių ir kiti taškai yra jūsų delne. Žinodami kokius taškus spaudyti jūs galėsite gydyti save kur bebūtumėt. Delnas - gydytojas visada su jumis. Skaudūs taškai delne rodo ne tik dabartines jūsų ligas, silpnas jūsų organizmo vietas, bet taip pat ir ateinančias ligas. Tiksliai veikiant taškus, įvairūs skausmai pradeda mažėti jau po kelių minučių. Be abejo reikia žinoti kaip, kiek laiko ir kokius taškus veikti. Tam nereikia išankstinio tikėjimo, bet būtina pabandyti, kad įsitikintumėt šios terapijos unikalumu. Už savo sveikatą esate atsakingi tik jūs patys. Ir taškų delnuose pagalba jūs galite efektyviai sau padėti spręsti savo sveikatos problemas, o gerai išmokę su džok terapijos principų galėsite ir kitiems pagelbėti.`,
   },
 };
