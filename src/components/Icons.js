f8ca5ec94281f69da0f6d3477fff3c051aafda36
Paulius Jacinkevicius <irgipaulius@gmail.com>
2026-02-19 22:54:30 +0300

    fix: replace @material-ui/icons with react-icons to fix React 18 peer dep conflict
    - Remove @material-ui/core and @material-ui/icons from package.json (both required React 16/17)
    - Migrate all 5 icon usages in Icons.js to react-icons (already installed)
    - Build size reduced by ~21 kB gzipped

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,15 +1,9 @@
 import React from "react";
 
-// Material Icons: https://v4.mui.com/components/material-icons/
-import CheckIcon from "@material-ui/icons/Check";
-import PhoneIcon from "@material-ui/icons/Phone";
-import MailIcon from "@material-ui/icons/Mail";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import MenuBookIcon from "@material-ui/icons/MenuBook";
-
 // React Icons: https://react-icons.github.io/react-icons/
 // md - Material Design icons
-import { MdCheck } from "react-icons/md";
+import { MdCheck, MdPhone, MdMail, MdMenuBook } from "react-icons/md";
+import { FaFacebook } from "react-icons/fa";
 
 export function MainCheckmarkRI() {
   const style = {
@@ -27,7 +21,7 @@ export function AboutCheckmark() {
   const style = {
     fontSize: "3rem",
   };
-  return <CheckIcon style={style} />;
+  return <MdCheck style={style} />;
 }
 
 export function MainCheckmark() {
@@ -37,9 +31,8 @@ export function MainCheckmark() {
     backgroundColor: "rgba(255, 122, 215, 0.85)",
     color: "white",
     borderRadius: "4px",
-    "&:hover": { color: "red" },
   };
-  return <CheckIcon style={style} />;
+  return <MdCheck style={style} />;
 }
 
 export function PhoneIconSvg() {
@@ -48,7 +41,7 @@ export function PhoneIconSvg() {
     marginRight: "1rem",
     verticalAlign: "middle",
   };
-  return <PhoneIcon style={style} />;
+  return <MdPhone style={style} />;
 }
 
 export function EmailIconSvg() {
@@ -58,7 +51,7 @@ export function EmailIconSvg() {
     marginRight: "0.8rem",
     verticalAlign: "middle",
   };
-  return <MailIcon style={style} />;
+  return <MdMail style={style} />;
 }
 
 export function FacebookIconSvg() {
@@ -67,7 +60,7 @@ export function FacebookIconSvg() {
     marginRight: "1rem",
     verticalAlign: "middle",
   };
-  return <FacebookIcon style={style} />;
+  return <FaFacebook style={style} />;
 }
 
 export function BlogIconSvg() {
@@ -76,5 +69,5 @@ export function BlogIconSvg() {
     marginRight: "1rem",
     verticalAlign: "middle",
   };
-  return <MenuBookIcon style={style} />;
+  return <MdMenuBook style={style} />;
 }
